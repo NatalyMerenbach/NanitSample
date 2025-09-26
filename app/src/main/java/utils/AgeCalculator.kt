0c81c5c6eacc3fea13cd96ad616dca4f33f11341
natalymerenbach <nataly@sureuniversal.com>
2025-09-26 16:59:26 +0300

    AgeCalculator text OLD added

--- app/src/main/java/utils/AgeCalculator.kt
+++ app/src/main/java/utils/AgeCalculator.kt
@@ -3,7 +3,6 @@ package utils
 import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneId
-import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 
 object AgeCalculator {
